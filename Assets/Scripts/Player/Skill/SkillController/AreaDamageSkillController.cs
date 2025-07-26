907196c54b8436d848214adab94c463c6be8270b
digital8150 <wptpgud@gmail.com>
2025-07-26 18:23:12 +0900

    fix : sphere -> capsule 로 주변 적 체크 변경
    근데 여전히 지형에 따라서 제대로 주변 적에게 공격하지 못하는 문제가 있음...

--- Assets/Scripts/Player/Skill/SkillController/AreaDamageSkillController.cs
+++ Assets/Scripts/Player/Skill/SkillController/AreaDamageSkillController.cs
@@ -5,6 +5,9 @@ public class AreaDamageSkillController : SkillController
     private float tickTimer;
     private Collider[] hitBuffer = new Collider[50];
 
+    //원기둥 판정 높이 상수
+    private const float CAPSULEHEIGHT = 20f;
+
     private void Update()
     {
         if (skillData == null || currentLevel == 0) return;
@@ -28,7 +31,12 @@ public class AreaDamageSkillController : SkillController
 
     private void PerformAreaDamage(AreaDamageSkillLevelData levelData)
     {
-        int numColliders = Physics.OverlapSphereNonAlloc(transform.position, levelData.radius, hitBuffer);
+        float capsuleHalfHeight = CAPSULEHEIGHT / 2;
+        Vector3 bottomOfCapsule = transform.position - (Vector3.up * capsuleHalfHeight);
+        Vector3 topOfCapsule = transform.position + (Vector3.up * capsuleHalfHeight);
+
+        int numColliders = Physics.OverlapCapsuleNonAlloc(bottomOfCapsule, topOfCapsule, levelData.radius, hitBuffer);
+        
         for (int i = 0; i < numColliders; i++)
         {
             Collider hit = hitBuffer[i];
@@ -38,6 +46,7 @@ public class AreaDamageSkillController : SkillController
                 if (enemy != null)
                 {
                     enemy.Damage(levelData.damagePerTick);
+                    Debug.Log($"마늘과 고추 공격 ({levelData.damagePerTick}) -> ({enemy.name})");
                 }
             }
         }
