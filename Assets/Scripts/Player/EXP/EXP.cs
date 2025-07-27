c84be58a5ba7c0f820ef9f84fedda2aa524bbc64
hyojoon Lim <hyojoon42@naver.com>
2025-07-27 22:38:47 +0900

    feature : 경험치 모두 습득 아이템 구현

--- Assets/Scripts/Player/EXP/EXP.cs
+++ Assets/Scripts/Player/EXP/EXP.cs
@@ -5,11 +5,15 @@ public class EXP : MonoBehaviour
     [Header("Ä¸½¶ °æÇèÄ¡·®")]
     public float exp;
 
+    [Header("²ø·Á¿À´Â È¿°ú ¼³Á¤ ¼Ó¼º")]
+
     private Renderer capsuleRenderer;       //Ä¸½¶ »ö
+    private Rigidbody rb;
 
     private void Awake()
     {
         capsuleRenderer = GetComponent<Renderer>();
+        rb = GetComponent<Rigidbody>();
     }
 
     //----------------------- ÇÃ·¹ÀÌ¾î - °æÇèÄ¡ Ä¸½¶ Á¢ÃË ------------------------
