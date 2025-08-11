ad66cefd40080f7a13e190b80809a199d263ef0b
Digital <73296205+digital8150@users.noreply.github.com>
2025-08-11 19:37:19 +0900

    fix: 마늘 스킬 보스 공격

--- Assets/Scripts/Player/Skill/SkillController/AreaDamageSkillController.cs
+++ Assets/Scripts/Player/Skill/SkillController/AreaDamageSkillController.cs
@@ -75,7 +75,7 @@ public class AreaDamageSkillController : SkillController
         for (int i = 0; i < numColliders; i++)
         {
             Collider hit = hitBuffer[i];
-            if (hit.CompareTag("Enemy"))
+            if (hit.CompareTag("Enemy") || hit.CompareTag("Boss"))
             {
                 Enemy enemy = hit.GetComponent<Enemy>();
                 if (enemy != null)
